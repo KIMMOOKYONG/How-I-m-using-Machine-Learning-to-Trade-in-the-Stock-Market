88343922e0f717eb8f8dfd2314cd06e960f2c4ae
KIMMOOKYONG <102650331+KIMMOOKYONG@users.noreply.github.com>
2022-04-06 22:48:54 +0900

    Update lr_inference.py

--- models/lr_inference.py
+++ models/lr_inference.py
@@ -4,6 +4,7 @@ author - Kaneel Senevirathne
 date - 1/13/2022
 """
 
+# doctest는 기본적으로 unittest, pytest처럼 테스를 위한 모듈이다.
 from doctest import OutputChecker
 import numpy as np
 import pickle
@@ -35,27 +36,49 @@ def _threshold(probs, threshold):
     return np.array(prob_thresholded)
 
 
+"""
+
+"""
 def LR_v1_predict(stock, start_date, end_date, threshold = 0.98):
     """
     this function predict given the data
+    주가 데이터를 기반으로 고가, 저가 예측
     """
     # create model and scaler instances 
+    # 모델, 스케일러 로딩
     scaler = load_scaler('v3')
     lr = load_LR('v3')
-    #create input
+    
+    # create input
+    # 로지스틱 회귀 테스트 데이터 생성
+    # 라벨이 붙어있지 않는 데이터
     data = create_test_data_lr(stock, start_date, end_date)
-    #get close price of final date
+    
+    # get close price of final date
+    # 테스트 데이터의 마지막 날 종가
     close_price = data['close'].values[-1]
-    #get input data to model
+    
+    # get input data to model
+    # 모델에 입력할 데이터, 종가 칼럼을 제거
     input_data = data[['volume', 'normalized_value', '3_reg', '5_reg', '10_reg', '20_reg']]
+    # 1D 데이터를 2D 데이터로 변환
+    # 마지막 날 데이터
     input_data = input_data.to_numpy()[-1].reshape(1, -1)
-    #scale input data
+    
+    # scale input data
+    # 마지막 날 데이터 스케일링, 정규화
     input_data_scaled = scaler.transform(input_data)
+    # 예측값
     prediction = lr._predict_proba_lr(input_data_scaled)
+    # 가중치 적용 예측값
     prediction_thresholded = _threshold(prediction, threshold)
    
+    # 예측값, 가중치 적용 예측값, 종가
     return prediction[:, 0], prediction_thresholded[0], close_price
 
+"""
+매도 
+"""
 def LR_v1_sell(stock, buy_date, buy_price, todays_date, sell_perc = 0.1, hold_till = 3, stop_perc = 0.05):
     """
     gets stock price. recommnd to sell if the stock price is higher sell_perc * buy_price + buy_price
@@ -65,16 +88,16 @@ def LR_v1_sell(stock, buy_date, buy_price, todays_date, sell_perc = 0.1, hold_ti
     sell_perc - sell percentage 
     hold_till - how many days to hold from today
     """
-    current_price = get_stock_price(stock, todays_date) #current stock value
-    sell_price = buy_price + buy_price * sell_perc
-    stop_price = buy_price - buy_price * stop_perc
-    sell_date = buy_date + timedelta(days = hold_till) #the day to sell 
+    current_price = get_stock_price(stock, todays_date) # current stock value, 현재가
+    sell_price = buy_price + buy_price * sell_perc # 매도 가격
+    stop_price = buy_price - buy_price * stop_perc # 손절 가격
+    sell_date = buy_date + timedelta(days = hold_till) # the day to sell 
     time.sleep(1) #to make sure the requested transactions per seconds is not exeeded.
     #some times it returns current price as none
     if (current_price is not None) and ((current_price < stop_price) or (current_price >= sell_price) or (todays_date >= sell_date)):
-        return "SELL", current_price #if criteria is met recommend to sell
+        return "SELL", current_price # if criteria is met recommend to sell
     else:
-        return "HOLD", current_price #if crieteria is not met hold the stock
+        return "HOLD", current_price # if crieteria is not met hold the stock
 
 
 
