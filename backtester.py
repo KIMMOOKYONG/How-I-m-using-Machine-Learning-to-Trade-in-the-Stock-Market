3e30baa0bd8c6ff18e6116888b9953e22946e3c8
KIMMOOKYONG <dharma6872@gamil.com>
2022-04-14 05:36:56 +0000

    백테스팅모듈업데이트

--- backtester.py
+++ backtester.py
@@ -201,6 +201,6 @@ if __name__ == "__main__":
     #모델버전
     model_version = "v2"
     stocks = list(np.unique(dow + other))
-    back = backtester(dow, LR_v1_predict, model_version, 3000, datetime(2021, 12, 1), datetime(2022, 4, 14), threshold = 0.9, sell_perc = 0.03, hold_till = 10,\
+    back = backtester(dow, LR_v1_predict, model_version, 3000, datetime(2021, 12, 1), datetime(2022, 4, 14), threshold = 0.98, sell_perc = 0.03, hold_till = 10,\
         stop_perc = 0.03)
     back.backtest()
